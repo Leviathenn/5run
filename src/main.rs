a0d74576e32edd405571e8c0b2c955c663339b8b
Leviathenn <levixanderson@gmail.com>
2024-04-26 22:09:22 -0400

    remade changes from local machine

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,93 @@
 /**
  * @author Leviathen
 **/
-
 use std::io::{stdin, stdout, Write};
 
-#[derive(Debug)]
-#[derive(Clone)]
-struct User{
-    name: String
+#[derive(Debug, Clone)]
+struct User {
+    name: String,
 }
-
-
 
 mod commands {
     use std::io::{stdin, stdout, Write};
 
     use crate::User;
 
-
-    pub fn users(args: Vec<&str>, users: &mut Vec<User>){
+    pub fn users(args: Vec<&str>, _users: &mut Vec<User>) {
         if args.len() == 1 {
             let mut index: u32 = 0;
-            for user in users {
-                print!("{} - {}",index,user.name);
+            for user in _users {
+                println!("{} - {}", index, user.name);
                 index += 1;
             }
-        }else{
+            
+        } else {
             if args.get(1).unwrap().to_string() == String::from("new") {
                 print!("name> ");
                 stdout().flush().expect("Unable to flush stdout.");
-            
                 let mut name = String::new();
-                stdin().read_line(&mut name).unwrap();
-                users.push(User { name: name });
-                        
+               stdin().read_line(&mut name).unwrap();
+                _users.push(User { name: String::from(name.trim()) });
             }
         }
     }
-    pub fn select(args: Vec<&str>, user: &mut User, users: &mut Vec<User>){
+    pub fn select(args: Vec<&str>, user: &mut User, _users: &mut Vec<User>) {
         if args.len() == 1 {
             print!("Invalid use of \"select\". Select takes 2 arguments");
-        }else{
+        } else {
             let selected: &str = args.get(1).unwrap();
-            match selected{
-                "user"=>{
-                    let s_user: usize = args.get(2).unwrap().parse().expect(format!("Expected int, got: \"{}\".",args.get(2).unwrap()).as_str());
-                    if let Some(selected_user) = users.get(s_user) {
-                        *user = selected_user.clone();
-                        print!("Selected User: {}",user.name);
+            match selected {
+                "user" => {
+                    let s_user: usize = args.get(2).unwrap().parse().expect(
+                        format!("Expected int, got: \"{}\".", args.get(2).unwrap()).as_str(),
+                    );
+                    if let Some(selected_user) = _users.get(s_user) {
+                        *user = selected_user.clone(); // Clone the selected_user into user
+                        println!("Selected User: {}", user.name.trim());
                     } else {
-                        print!("User index out of bounds.");
+                        println!("User index out of bounds.");
                     }
                 }
-                _ =>{}
+                _ => {}
             }
         }
-
     }
-    pub fn selected(args: Vec<&str>, users: &mut Vec<User>,scat: &mut String){
-        
-    }
-
-}
-
-fn main(){
-    let mut users: Vec<User> = vec![];
-    let mut selected_user: User = User { name: String::from("") };
-    let mut scat: String = String::new();
-    loop {
     
-    
-        print!("> ");
-        stdout().flush().expect("Unable to flush stdout.");
-    
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
-    
-        let parts: Vec<_> = input.trim().split_whitespace().collect();
-        let command:&str = parts.get(0).unwrap_or(&&"errornocmd");
-        let args: Vec<&str> = parts;
-        
-        match command {
-            "users"=>{commands::users(args,&mut users)},
-            "select"=>{commands::select(args, &mut selected_user, &mut users)}
-            "selected"=>{commands::selected(args,&mut users,&mut scat)}
-            "errornocmd"=>{}
-            _=>{println!("\"{}\" Is not a valid command.",command)}
-            
+    pub fn selected(_args: Vec<&str>, _users: &mut Vec<User>, scat: &mut String, selected: &mut User) {
+        if scat == &mut String::from("user") {
+            println!("Selected user \"{}\".", selected.name.trim());
+        } else if scat == &mut String::new(){
+            println!("There is no user selected.");
         }
     }
-} 
+}
+
+fn main() {
+    let mut users: Vec<User> = vec![];
+    let mut selected_user: User = User {
+        name: String::from(""),
+    };
+    let mut scat: String = String::new();
+    loop {
+        print!("> ");
+        stdout().flush().expect("Unable to flush stdout.");
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let parts: Vec<_> = input.trim().split_whitespace().collect();
+        let command: &str = parts.get(0).unwrap_or(&&"errornocmd");
+        let args: Vec<&str> = parts;
+    
+        match command {
+            "users" => commands::users(args, &mut users),
+            "select" => commands::select(args, &mut selected_user, &mut users),
+            "selected" => commands::selected(args, &mut users, &mut scat, &mut selected_user),
+            "errornocmd" => {}
+            _ => {
+                println!("\"{}\" Is not a valid command.", command.trim())
+                
+            }
+        }
+    }
+}
